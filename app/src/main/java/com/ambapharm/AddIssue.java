84aa62beaa3fe8c5a6736b3da8a1b0dd2dd84e40
Achrafvin <achraf.derkaoui@emsi-edu.ma>
2023-10-21 11:12:22 +0100

    Init

---
+++ app/src/main/java/com/ambapharm/AddIssue.java
@@ -0,0 +1,14 @@
+package com.ambapharm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddIssue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_issue);
+    }
+}
---
+++ app/src/main/java/com/ambapharm/UpdateFnc.java
@@ -0,0 +1,14 @@
+package com.ambapharm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateFnc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.update_fnc);
+    }
+}
